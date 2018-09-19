b968f1a4bd2a6b12d23c2a1623c8b2b3893b6dc2
Patrick Dolloso <patrick.dolloso@gmail.com>
2018-09-19 02:37:27 -0400

    commit

---
+++ W2/PAU2/t_area.py
@@ -0,0 +1,13 @@
+# Patrick Dolloso s136699 CS1101 Programming Assignment Unit 2
+
+# Assign the value 12 to the base variable:
+base = 12
+
+# Assign the value 16 to the height value:
+height = 16
+
+# compute area based on formula area = 1/2*base*height
+
+area = 0.5*base*height
+
+print('the area of the triangle is: ' + str(area))
